e3bcfb070ee933bdb8473b70c6b735c41ad97257
Todd Bittner <toddbittner@gmail.com>
2023-04-28 21:45:44 +0000

    Added config changes for svelte.

--- replit.nix
+++ replit.nix
@@ -1,6 +1,7 @@
 { pkgs }: {
 	deps = [
-		pkgs.nodejs-16_x
+		pkgs.less
+  pkgs.nodejs-16_x
         pkgs.nodePackages.typescript-language-server
 		pkgs.nodePackages.svelte-language-server
         pkgs.yarn
