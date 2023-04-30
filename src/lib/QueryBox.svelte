07d09d234a37ae4a4d0ec7d77847b6de8f65b5e7
trbittner <2094713+trbittner@users.noreply.github.com>
2023-04-30 14:16:17 +0000

    Simplified the logic for QueryBox and stripped the test form.

--- src/lib/QueryBox.svelte
+++ src/lib/QueryBox.svelte
@@ -1,26 +1,9 @@
-<script>
-  import { createEventDispatcher } from 'svelte';
-  const dispatch = createEventDispatcher();
-  
-  function queryInput(e) {
-    const formData = new FormData(e.target)
-    const data = {}
-    for (let field of formData) {
-      const [key,value] = field;
-      data[key] = value;
-    }
-    dispatch('querySubmit',{'data': data['query']});
-  }
-</script>
-
-<form id="form" on:submit|preventDefault="{queryInput}">
-  <input
-    name="query"
-    id="query"
-    placeholder="Type your message here"
-  />
-  <button type="submit" id="send"><i class="bi bi-send-fill"></i></button>
-</form>
+<input
+  name="query"
+  id="query"
+  placeholder="Type your message here"
+/>
+<button type="submit" id="send"><i class="bi bi-send-fill"></i></button>
 
 <style>
   /* A cellphone on the smaller side is 640px x 360px */
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,10 @@
 <script>
   import BouncingDots from "$lib/BouncingDots.svelte";
   import QueryBox from "$lib/QueryBox.svelte";
-
-  function handleQuery(event) {
-    console.log(event.detail.data);
-  }
 </script>
 
 <h1>Hello Sveltekit</h1>
 <p>This is a dead simple sveltekit template.</p>
 
 <BouncingDots />
-<QueryBox on:querySubmit={handleQuery} />
-<form method="POST">
-  <h1>Test</h1>
-  <input name="description" />
-</form>
+<QueryBox />
