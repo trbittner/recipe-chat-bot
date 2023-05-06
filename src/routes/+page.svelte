f584be2767bbaa53993648178281b4a4a68c2aa5
trbittner <2094713+trbittner@users.noreply.github.com>
2023-05-06 19:31:03 +0000

    Added changes to main page for GUI.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,44 @@
 <script>
-  import BouncingDots from "$lib/BouncingDots.svelte";
-  import QueryBox from "$lib/QueryBox.svelte";
+  import BouncingDots from "$lib/components/BouncingDots.svelte";
+  import QueryBox from "$lib/components/QueryBox.svelte";
 
   let query = '';
 
   function handleQuery() {
-  
+    console.log(data.idx.search(query))
   }
 
   export let data;
-  console.log(data.idx.search("mozzarella"))
 </script>
 
 <h1>Hello Sveltekit</h1>
-<p>This is a dead simple sveltekit template.</p>
 
+<div class="chatbot-bubble bot">Hello</div>
+<div class="chatbot-bubble user">Hi there</div>
 <BouncingDots />
 <QueryBox bind:queryInput={query} on:sendQuery={handleQuery}/>
+
+<style>
+  .chatbot-bubble {
+    max-width: 80%;
+    border-radius: 20px;
+    padding: 10px 15px;
+    font-size: 16px;
+    line-height: 1.5;
+    display: inline-block;
+  }
+
+  .chatbot-bubble.user {
+    background-color: #007bff;
+    color: #fff;
+    float: right;
+    margin-bottom: 10px;
+  }
+
+  .chatbot-bubble.bot {
+    background-color: yellow;
+    color: #000;
+    float: left;
+    margin-bottom: 10px;
+  }
+</style>
