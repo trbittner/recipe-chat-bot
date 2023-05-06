ad7dc4ee63d47cc6522138c632f8d4bca831ef73
trbittner <2094713+trbittner@users.noreply.github.com>
2023-05-06 19:37:13 +0000

    Added a chat bubble  component

---
+++ src/lib/components/ChatBubble.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let speaker;
+  export let text;
+</script>
+
+<div class="chatbot-bubble {speaker}">{text}</div>
+
+<style>
+  .chatbot-bubble {
+    max-width: 80%;
+    border-radius: 20px;
+    padding: 10px 15px;
+    font-size: 16px;
+    line-height: 1.5;
+    display: inline-block;
+  }
+
+  .chatbot-bubble.user {
+    background-color: #007bff;
+    color: #fff;
+    float: right;
+    margin-bottom: 10px;
+  }
+
+  .chatbot-bubble.bot {
+    background-color: yellow;
+    color: #000;
+    float: left;
+    margin-bottom: 10px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import BouncingDots from "$lib/components/BouncingDots.svelte";
   import QueryBox from "$lib/components/QueryBox.svelte";
+  import ChatBubble from "$lib/components/ChatBubble.svelte";
 
   let query = '';
 
@@ -13,32 +14,7 @@
 
 <h1>Hello Sveltekit</h1>
 
-<div class="chatbot-bubble bot">Hello</div>
-<div class="chatbot-bubble user">Hi there</div>
+<ChatBubble speaker="bot" text="Hello" />
+<ChatBubble speaker="user" text="Hi there" />
 <BouncingDots />
 <QueryBox bind:queryInput={query} on:sendQuery={handleQuery}/>
-
-<style>
-  .chatbot-bubble {
-    max-width: 80%;
-    border-radius: 20px;
-    padding: 10px 15px;
-    font-size: 16px;
-    line-height: 1.5;
-    display: inline-block;
-  }
-
-  .chatbot-bubble.user {
-    background-color: #007bff;
-    color: #fff;
-    float: right;
-    margin-bottom: 10px;
-  }
-
-  .chatbot-bubble.bot {
-    background-color: yellow;
-    color: #000;
-    float: left;
-    margin-bottom: 10px;
-  }
-</style>
