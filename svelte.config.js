fa189a50c761ec451510eb04005ef996357687e5
trbittner <2094713+trbittner@users.noreply.github.com>
2023-05-14 15:22:40 +0000

    Added ability to process md files.

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,18 @@
 import adapter from '@sveltejs/adapter-node';
+import { mdsvex } from 'mdsvex';
 
 const config = {
 	kit: {
 		adapter: adapter()
-	}
+	},
+
+  extensions: ['.svelte','.md'],
+
+  preprocess: [
+    mdsvex({
+      extensions: ['md'],
+    })
+  ],
 };
 
 export default config;
