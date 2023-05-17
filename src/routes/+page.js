478f43782cc776ffaf4b115859d1c9a1d02b240d
trbittner <2094713+trbittner@users.noreply.github.com>
2023-05-17 21:08:54 +0000

    Finalized indexing logic for the site.

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,25 +1,21 @@
 import lunr from "lunr";
 import recipeObj from '$lib/data/recipes.json';
 
-export async function load({ fetch }) {
-  const recipeFiles = import.meta.glob('/src/routes/recipes/*.md')
-  for (const path in recipeFiles) {
-    const recipePath = path.slice(11,-3)
-    const response = await fetch(recipePath)
-    const data = await response.text()
-    console.log(data)
-  }
-  
+export async function load() {
   let idx = lunr(function() {
-    this.ref('name');
-    this.field('title');
+    this.ref('id');
+    this.field('name');
     this.field('ingredients');
-    this.field('categories');
+    this.field('tags');
 
     recipeObj.data.forEach(function (recipe) {
-        this.add(recipe);
+      this.add({
+        'id':recipe.id,
+        'name':recipe.name,
+        'ingredients': recipe.ingredients.join(' '),
+        'tags': recipe.tags,
+      });
     },this)
   })
-
   return {idx};
 }
--- src/routes/recipes/[slug]/+page.js
+++ src/routes/recipes/[slug]/+page.js
@@ -2,9 +2,9 @@ import recipeObj from '$lib/data/recipes.json';
 
 export function load({ params }) {
   console.log(params.slug)
-  for (let i = 0; i < recipeObj.data.length; i++) {
-    if (recipeObj.data[i].id === params.slug) {
-      return recipeObj.data[i];
+  for (let recipe of recipeObj.data) {
+    if (recipe.id === params.slug) {
+      return recipe;
     }
   }
 }
