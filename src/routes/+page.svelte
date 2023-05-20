10a4927739a4d31c46478673cb0c1d81ce4cbb90
trbittner <2094713+trbittner@users.noreply.github.com>
2023-05-20 15:30:21 +0000

    Added basic nav bar functionality and styling.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,15 +5,22 @@
   export let data;
 </script>
 
+<header>
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Recipes</a></li>
+      <li><a href="#">Categories</a></li>
+      <li><a href="#">About Us</a></li>      
+    </ul>
+  </nav>
+</header>
+
 <main>
   <section class="hero">
-    <div class="hero-image">
-      
-    </div>
+    <div class="hero-image"></div>
   </section>
 </main>
-
-<h1>Hello RecipeBot</h1>
 
 <div class='container'>
   <ChatCanvas data={data} />
@@ -21,6 +28,8 @@
 <BouncingDots />
 
 <style>
+  @import url('https://fonts.googleapis.com/css?family=Mallanna');
+  
   .container {
     display: flex;
     justify-content: center;
@@ -32,5 +41,36 @@
     background-position: center;
     height: 400px;
     position: relative;
+  }
+
+  nav {
+    font-family: "Mallanna", sans-serif;
+    font-size: 1.25em;
+    border-top: 5px solid;
+    border-bottom: 5px solid;
+    background-color: #fbeaab;
+    margin: 5px;
+  }
+
+  nav a {
+    text-decoration: none;
+  }
+
+  nav ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: space-around;
+  }
+
+  nav ul li a {
+    color: #454545;
+    font-weight: bold;
+    transition: color 0.5s ease;
+  }
+
+  nav ul li a:hover {
+    color: gold;
   }
 </style>
