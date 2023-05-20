e5dcdec50a68e9b129b084c21d53197dab5fefbb
trbittner <2094713+trbittner@users.noreply.github.com>
2023-05-20 13:53:26 +0000

    Started building out the homepage by adding the logo and hero to the main page.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
 <script>
   import BouncingDots from "$lib/components/BouncingDots.svelte";
-  import ChatCanvas from "../lib/components/ChatCanvas.svelte";
+  import ChatCanvas from "$lib/components/ChatCanvas.svelte";
 
   export let data;
-  
 </script>
+
+<main>
+  <section class="hero">
+    <div class="hero-image">
+      
+    </div>
+  </section>
+</main>
 
 <h1>Hello RecipeBot</h1>
 
@@ -17,5 +24,13 @@
   .container {
     display: flex;
     justify-content: center;
+  }
+
+  .hero-image {
+    background-image: url('/images/todds-bistro-logo.png');
+    background-size: cover;
+    background-position: center;
+    height: 400px;
+    position: relative;
   }
 </style>
